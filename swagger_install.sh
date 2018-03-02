b1918614696fe7ed13bf20dfe57a72d73b618a74
zhacker98 <zhacker98@users.noreply.github.com>
2018-03-02 10:46:38 -0500

    Create swagger_install.sh

---
+++ swagger_install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+latestv=$(curl -s https://api.github.com/repos/go-swagger/go-swagger/releases/latest | jq -r .tag_name)
+curl -o /usr/local/bin/swagger -L'#' https://github.com/go-swagger/go-swagger/releases/download/$latestv/swagger_$(echo `uname`|tr '[:upper:]' '[:lower:]')_amd64
+chmod +x /usr/local/bin/swagger
